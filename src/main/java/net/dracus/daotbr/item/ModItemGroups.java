eadd083f43f369673033ed135c65de9cf5e826c5
Michael <giordanomichael2@gmail.com>
2026-07-02 21:23:11 -0400

    pre-release v.0.1.0 (syringes added)

---
+++ src/main/java/net/dracus/daotbr/item/ModItemGroups.java
@@ -0,0 +1,35 @@
+package net.dracus.daotbr.item;
+
+import net.dracus.daotbr.DAOTBR;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup SHIFTER_SYRINGES_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(DAOTBR.MOD_ID, "shifter_syringes_items"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.ATTACK_SYRINGE))
+                    .displayName(Text.translatable("itemgroup.daotbr.shifter_syringes_items"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModItems.ACKERMAN_SYRINGE);
+                        entries.add(ModItems.ARMORED_SYRINGE);
+                        entries.add(ModItems.ATTACK_SYRINGE);
+                        entries.add(ModItems.BEAST_SYRINGE);
+                        entries.add(ModItems.CART_SYRINGE);
+                        entries.add(ModItems.COLOSSAL_SYRINGE);
+                        entries.add(ModItems.FEMALE_SYRINGE);
+                        entries.add(ModItems.JAW_SYRINGE);
+                        entries.add(ModItems.WARHAMMER_SYRINGE);
+                    })
+
+                    .build());
+
+
+    public static void registerItemGroups() {
+        DAOTBR.LOGGER.info("Registering Item Groups for " + DAOTBR.MOD_ID);
+    }
+}
