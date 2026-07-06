745208ae3073e0c46ec6ac56ca943c5084be9c8f
Xavier Kelly <xkel@github.com>
2026-07-06 17:51:26 -0400

    Add auto-spectator on death, reduce fabric loader version floor requirement

--- src/main/java/net/dracus/daotbr/DAOTBR.java
+++ src/main/java/net/dracus/daotbr/DAOTBR.java
@@ -4,6 +4,17 @@ import net.dracus.daotbr.item.ModItemGroups;
 import net.dracus.daotbr.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.GameMode;
+import net.minecraft.world.World;
+
 import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
@@ -19,7 +30,35 @@ public class DAOTBR implements ModInitializer {
 		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 
+
+		// Two apparent ways to do things on death, using the entity events or a mixin IE // Source - https://stackoverflow.com/a/73941878
+		// @Inject(method="onDeath", at=@At("TAIL"))
+		// This uses Server Entity Events
+
+		ServerLivingEntityEvents.AFTER_DEATH.register((LivingEntity entity, DamageSource source) -> {
+			if (entity instanceof ServerPlayerEntity player) {
+				MinecraftServer server = player.getServer();
+				if (server == null) return;
+
+				String name = player.getName().getString();
+
+				// Resolve the target dimension. If this logs a warning, the ID is wrong
+				RegistryKey<World> paradisKey =
+						RegistryKey.of(RegistryKeys.WORLD, Identifier.of("dannys-aot", "paradis"));
+				ServerWorld paradis = server.getWorld(paradisKey);
+				if (paradis == null) {
+					LOGGER.warn("Dimension {} not found — cannot teleport {}", paradisKey.getValue(), name);
+				}
+
+				player.teleport(paradis, 0, 64, 0, java.util.Set.of(), 0, 0);
+				player.changeGameMode(GameMode.SPECTATOR);
+				server.getCommandManager().executeWithPrefix(server.getCommandSource(), "team leave " + name);
+
+				LOGGER.info("{} died, teleported to {}, and was set to spectator", name, paradisKey.getValue());
+			}
+		});
 	}
+
 
 	public static Identifier id(String path) {
 		return Identifier.of(MOD_ID, path);
